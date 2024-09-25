41e8cbfb8ab97dd5209b3456766a2f51303e5280
DavidLuques <braian.luques2@gmail.com>
2024-09-25 12:13:40 -0300

    uh

--- app/Models/post.php
+++ app/Models/post.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class post extends Model
+class Post extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+    protected $table = 'post'; // Especifica que el modelo usa la tabla 'post'
+
     
-    protected $table = 'post';
+    protected function title():Attribute{
+        return Attribute::make(
+            set: function($value){
+                return strtolower($value);
+            }
+        );
+    } 
 }
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,29 @@ Route::get('/post', [PostController::class, 'post1']);
 
 Route::get('prueba', function () {
 
-    // $post = new Post;
-    // $post->title='titulo de test 1';
-    // $post->content='aca hay contenido je';
+    // CREAR REGISTRO
+    $post = new Post;
+    $post->title='titulo de tPRUEBA test 1';
+    $post->content='aca hay contenido je';
+    $post->save();
+    return $post;
+
+
+    //ACTUALIZAR REGISTRO
+    // $post = Post::find(1);
+    // $post = Post::where('id','1')->first();
+    // $post->title= 'yo soy el post1 debo ser distinto';
     // $post->save();
     // return $post;
 
 
-    // $post = Post::find(1);
+    // $post = Post::where('id','>=','2')->get();
+    // return $post;
 
-    $post =
-    return $post;
+    // $post = Post::find(1);
+    // $post->delete();
+    // return "eliminado eficientemente";
+
+
+
 });
